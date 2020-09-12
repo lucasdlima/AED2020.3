9e10fa906ba53630a9ebf40d602a5d24b769af37
Lucas de Lima <lucasdls.py@gmail.com>
2020-09-12 07:49:02 -0300

    clear

--- listaligada.py
+++ listaligada.py
@@ -53,16 +53,3 @@ class ListaLigada:
             ponteiro.data = value
         else:
             raise IndexError("Azedou aqui na __setitem__")
-
-
-'''def reverse_lst(lst):          #A Lista deve ter a função __setitem__ troca de elementos
-    tam = int(lst._tam)        #tamanho da lista
-    aux = tam - 1              #ultimo indice da lista
-    meio = tam//2              #meio da lista
-    for i in range(meio):      #usando o meio da lista, pois é igual ao n° de trocas necessarias
-        derradeiro = lst[aux]
-        lst[aux] = lst[i]      #ultimo indice da lista recebe o primeiro
-        lst[i] = derradeiro    #e o primeiro recebe o ultimo, assim a troca é feita.
-        aux -= 1               #agora com o penultimo até chegar ao meio.
-    return lst                 #lista invetida, sogra desce!!
-'''
